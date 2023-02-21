54d58bb7743800e06065dafd184e8e39656f2482
kubracennet <kubracennetyavasoglu@gmail.com>
2023-02-22 00:20:34 +0300

    Initial Commit

---
+++ RegularRoutine/RegularRoutineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegularRoutineApp.swift
+//  RegularRoutine
+//
+//  Created by Kübra Cennet Yavaşoğlu on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct RegularRoutineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
